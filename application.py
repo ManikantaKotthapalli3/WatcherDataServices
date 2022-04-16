f389771e8e80c799d4e6cf046d2542384f3c9d1d
ManiKanta <manikantakotthapalli@hotmail.com>
2022-04-16 07:49:03 -0700

    Added a base api to get most popular titles

--- application.py
+++ application.py
@@ -1,25 +1,17 @@
-from flask import Flask
 import mysql.connector
+from flask import Flask
+import json
 
 app = Flask(__name__)
 
 
-def get_remote_data_base_connection():
-    return mysql.connector.connection(
-        host="129.146.128.174",
-        port="3306",
-        user="mysqlMaster",
-        password="Master@123"
-    )
-
-
 def get_db_connection():
     try:
-        db = get_remote_data_base_connection()
-        my_cursor = db.cursor()
-        my_cursor.excute("SELECT VERSION()")
-        return my_cursor.fetchone()
+        db = mysql.connector.connect(user="mysqlMaster", password="Master@123",
+                                      host="watcherdatabse.mysql.database.azure.com", port=3306,
+                                      database="watcherdatabse", ssl_ca="{ca-cert filename}", ssl_disabled=False)
 
+        return db
     except Exception as e:
         print('DB connection error', e)
         return None
@@ -27,13 +19,42 @@ def get_db_connection():
 
 @app.route("/testDBConnection/")
 def test_db_connection():
+    db = get_db_connection()
 
-    results = get_db_connection()
+    if db:
+        my_cursor = db.cursor()
+        my_cursor.execute("SELECT VERSION()")
+        results = my_cursor.fetchone()
+    else:
+        results = None
 
     if results:
         return "DB connection successful"
     else:
         return "DB connection is unsuccessful"
+
+
+@app.route("/getMostPopularTitles/")
+def get_most_popular_titles():
+    db = get_db_connection()
+
+    try:
+        if db:
+            cursor = db.cursor()
+            cursor.execute("select * from `watcherdatabse`.`MostPopularTitlesDaily`")
+            row_headers = [x[0] for x in cursor.description]
+            out_put = cursor.fetchall()
+
+            json_data = []
+            for result in out_put:
+                json_data.append(dict(zip(row_headers, result)))
+            return json.dumps(json_data, default=str)
+        else:
+            return ''
+
+    except Exception as e:
+        return "Ran into an exception while reading"
+
 
 
 @app.route("/get/")
@@ -43,4 +64,3 @@ def hello():
 
 if __name__ == '__main__':
     app.run()
-
